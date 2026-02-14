47906bb0bdd4ffbf609b1304187e5a6d25566e6d
numanMushtaqDar <numanmushtaqahmaddar@gmail.com>
2026-02-14 15:19:19 +0530

    Day 2 - Finished remaining tasks

--- day_2/ariables.py
+++ day_2/ariables.py
@@ -12,10 +12,41 @@ is_true = True
 is_light_on = False
 
 
-first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on = "Numan", "Mushtaq", "Numan Mushtaq", "India", "Srinagar", 22, 2026, False, True, False
+
 
 print(type(first_name))
 print(type(age))
 print(type(is_married))
 print(type(is_light_on))
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one/num_two
+remainder = num_two%num_one
+exp = num_one**num_two
+floor_division = num_one//num_two
+
+radius = 30
+
+area_of_circle = 3.14*radius*radius
+circum_of_circle = 2*3.14*radius
+
+Radius = float(input("Enter the Radius of the circle: "))
+Area = 3.14*Radius*Radius
+print(f"The Area of the Circle with Radius {Radius} = {Area}")
+
+first_name = input("Enter your first name: " )
+last_name = input("Enter your last name: ")
+country = input("Enter from which country do you belong to: ")
+age = input("Enter your age: ")
+
+
+
 
